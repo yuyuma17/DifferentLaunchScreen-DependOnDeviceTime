5ab8e0cc3fa2a2c5ca99d00159d13cd99de8121e
Lacie <dark830917@gmail.com>
2019-10-07 17:56:19 +0800

    Initial Commit

---
+++ DifferentLaunchScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DifferentLaunchScreen
+//
+//  Created by 黃士軒 on 2019/10/7.
+//  Copyright © 2019 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
