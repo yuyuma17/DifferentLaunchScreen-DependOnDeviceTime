108fc6e2c980131b6e68faf4f008122bff3c02ee
Lacie <dark830917@gmail.com>
2019-10-08 11:31:48 +0800

    code is ok. next is demo and readme

--- DifferentLaunchScreen/ViewController.swift
+++ DifferentLaunchScreen/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animation()
     }
-
-
+    
+    func getDeviceHour() -> Int {
+        dateFormatter.dateFormat = "HH"
+        return Int(dateFormatter.string(from: Date()))!
+    }
+    
+    private func animation() {
+        guard let launchScreen = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController() else {return}
+        self.view.addSubview(launchScreen.view)
+        
+        if let image = launchScreen.view.viewWithTag(1) as? UIImageView {
+            if getDeviceHour() >= 18 || getDeviceHour() < 7 {
+                image.image = UIImage(named: "evening")
+            } else {
+                image.image = UIImage(named: "morning")
+            }
+        }
+        
+        if let label = launchScreen.view.viewWithTag(2) as? UILabel {
+            if getDeviceHour() >= 18 || getDeviceHour() < 7 {
+                label.text = "Good evening~~"
+            } else {
+                label.text = "Good morning~~"
+            }
+            UIView.animate(withDuration: 5,
+                           delay: 2,
+                           options: .curveEaseInOut,
+                           animations: {
+                            label.transform = CGAffineTransform(rotationAngle: .pi)
+                            label.transform = .identity
+                            launchScreen.view.alpha = 0
+            }) { (finished) in
+                launchScreen.view.removeFromSuperview()
+            }
+        }
+    }
 }
 
